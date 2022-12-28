a1b7ef54d8d0c355194e143247934fcafef43bfb
CcSimple <840054486@qq.com>
2022-12-28 02:08:22 +0000

    Deploying to gh-pages from @ CcSimple/sv-print-docs@d0184f1160c6ff6e7d4b92227a9d82c5e3061431 🚀

---
+++ assets/mermaid-mindmap.esm.min-de09359a.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-ba929b3b.js";import"./framework-bba6090e.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
