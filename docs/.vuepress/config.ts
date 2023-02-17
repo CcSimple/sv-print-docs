6da760165a494714c115ea137d4256a62f61dd91
Canim <840054486@qq.com>
2023-02-17 09:42:32 +0800

    fix doc search

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -23,53 +23,53 @@ export default defineUserConfig({
 
   plugins: [
     sitemapPlugin({
-      hostname: "https://ccsimple.github.io/sv-print-docs/"
+      hostname: "https://ccsimple.github.io/sv-print-docs/",
     }),
     docsearchPlugin({
       appId: "EK98MFQYBD",
-      apiKey: "214ef492c27bef3611fca424d60126f0",
+      apiKey: "591323234172ec0d4b26c7e21754c7e9",
       indexName: "sv-print",
       locales: {
         "/": {
           placeholder: "搜索文档",
           translations: {
             button: {
-              buttonText: '搜索文档',
-              buttonAriaLabel: '搜索文档',
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
             },
             modal: {
               searchBox: {
-                resetButtonTitle: '清除查询条件',
-                resetButtonAriaLabel: '清除查询条件',
-                cancelButtonText: '取消',
-                cancelButtonAriaLabel: '取消',
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
               },
               startScreen: {
-                recentSearchesTitle: '搜索历史',
-                noRecentSearchesText: '没有搜索历史',
-                saveRecentSearchButtonTitle: '保存至搜索历史',
-                removeRecentSearchButtonTitle: '从搜索历史中移除',
-                favoriteSearchesTitle: '收藏',
-                removeFavoriteSearchButtonTitle: '从收藏中移除',
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
               },
               errorScreen: {
-                titleText: '无法获取结果',
-                helpText: '你可能需要检查你的网络连接',
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
               },
               footer: {
-                selectText: '选择',
-                navigateText: '切换',
-                closeText: '关闭',
-                searchByText: '搜索提供者',
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
               },
               noResultsScreen: {
-                noResultsText: '无法找到相关结果',
-                suggestedQueryText: '你可以尝试查询',
-                reportMissingResultsText: '你认为该查询应该有结果？',
-                reportMissingResultsLinkText: '点击反馈',
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
               },
             },
-          }
+          },
         },
         // "/en": {
         //   placeholder: "Search Docs",
